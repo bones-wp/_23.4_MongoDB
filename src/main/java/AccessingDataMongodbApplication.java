3e025cc2802049354725d940dc7e9c371af3b765
Yuriy <j-thrash@ya.ru>
2022-06-30 14:11:12 +0300

    1

---
+++ src/main/java/AccessingDataMongodbApplication.java
@@ -0,0 +1,35 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class AccessingDataMongodbApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public void run(String... args) throws Exception {
+        repository.deleteAll();
+
+        // Добавляем несколько пользователей
+        repository.save(new User("Tester", "Testerov","test@test.com","+1005224"));
+        repository.save(new User("SecondUser", "His latName","test@test.com","+795001"));
+
+        // Находим всех пользователей
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (User user : repository.findAll()) {
+            System.out.println(user);
+        }
+        System.out.println();
+
+        // Ищем конкретного пользователя
+        System.out.println("Customer found with findByFirstName('Tester'):");
+        System.out.println("--------------------------------");
+        System.out.println(repository.findByFirstName("Tester"));
+
+        System.out.println("Customers found with findByEmail('test@test.com'):");
+        System.out.println("--------------------------------");
+        for (User User : repository.findByEmail("test@test.com")) {
+            System.out.println(User);
+        }
+    }
+
+}
