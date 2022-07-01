e6d53a0a052e728ea1e6795a1098d9727a173648
Osmolovskiy Yuriy <j-thrash@ya.ru>
2022-07-01 20:35:07 +0300

    23.5

--- src/main/java/sf/MongoDB/repositiry/UserRepository.java
+++ src/main/java/sf/MongoDB/repositiry/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface UserRepository extends MongoRepository <User, String> {
     public User findByFirstName(String firstName);
     public List<User> findByEmail(String email);
+    Long deletePersonByfirstName (String firstName);
 
 }
